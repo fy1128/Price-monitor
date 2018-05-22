f7a9a6d6b8a516e060003f1abfc153743687f787
fy1128 <892973009@qq.com>
2018-05-22 15:17:39 +0800

    archives mails by users.

--- PriceMonitor/monitor_main_js.py
+++ PriceMonitor/monitor_main_js.py
@@ -96,15 +96,15 @@ class Entrance(object):
             curr_item = dict(item)
             
             #check whether field was updated.
-            curr_item['status'] = 1
+            curr_item['updated'] = 1
 
             name = cr.get_name_jd()
-            status = sq.update_item_name(column_id, name)
+            updated = sq.update_item_name(column_id, name)
             curr_item['item_name'] = name
 
             prices = cr.get_price_jd()
             # tunple current price, discount
-            status = price = sq.update_item_price(column_id, prices)
+            updated = price = sq.update_item_price(column_id, prices)
             if not price:
                 curr_item['item_price'] = curr_item['discount'] = False
             else:
@@ -112,7 +112,7 @@ class Entrance(object):
                 curr_item['discount'] = price[1]
 
             subtitle = cr.get_subtitle_jd()
-            status = sq.update_item_subtitle(column_id, subtitle)
+            updated = sq.update_item_subtitle(column_id, subtitle)
             curr_item['subtitle'] = subtitle
             
             ext = {}
@@ -126,11 +126,11 @@ class Entrance(object):
             if huihui_info:  # skip this if not crawled
                 sq.update_item_max_price(column_id, huihui_info[0])
                 sq.update_item_min_price(column_id, huihui_info[1])
-                status = curr_item['highest_price'] = huihui_info[0]
-                status = curr_item['lowest_price'] = huihui_info[1]
+                updated = curr_item['highest_price'] = huihui_info[0]
+                updated = curr_item['lowest_price'] = huihui_info[1]
 
-            if not status:
-                curr_item['status'] = 0
+            if not updated:
+                curr_item['updated'] = 0
 
             return curr_item
 
@@ -147,56 +147,87 @@ class Entrance(object):
         # Send email in a loop, avoid sending simultaneously.
         sq = Sql()
         items = sq.check_item_need_to_remind(prev_items, curr_items)
-        logging.warning('This loop sent email: %s', items)
 
+        items_stdout =[]
+        mail_map = {}
         for item in items[0]:  # email, item_name, item_price, user_price, item_id, column_id
+            if item[0] not in mail_map:
+                mail_map[item[0]] = {'mon': {'ids': [], 'msg': []}}
+
+            # skip duplicate item in items_need_mail
+            if item[7] in mail_map[item[0]]['mon']['ids']:
+                continue
+
+            mail_map[item[0]]['mon']['ids'].append(item[7])
+                
             item_url = 'https://item.jd.com/' + str(item[6]) + '.html'
-            email_text = '物品：' + item[1] + '，\n' + \
-                         '上次监控价格为：' + str(item[3]) + '，\n' + \
+            email_text = ', '.join(item[10]) + '！\n' + \
+                         '物品：' + item[1] + '，\n' + \
                          '现在价格为：' + str(item[4]) + '，\n' + \
+                         '上次监控价格为：' + str(item[3]) + '，\n' + \
                          '您设定的价格为：' + str(item[5]) + '，赶紧购买吧！\n' + \
                          '子标题：' + item[2] + '，\n' + \
                          '历史最高价参考：' + str(item[8]) + '，\n' + \
                          '历史最低价参考：' + str(item[9]) + '，\n' + \
                          item_url
-            email_subject = ', '.join(item[10]) + '！您监控的物品有变更！'
-            try:
-                send_email = Mail(email_text, 'admin', 'user', email_subject, item[0])
-                send_email.send()
-                time.sleep(Email_TIME)
-            except:
-                logging.critical('Sent email failure, skip in this loop: %s', item[0])
-                continue
-            sq.update_status(item[7])
-            logging.warning('Sent email SUCCESS: %s', item[0])
-            
+
+            items_stdout.append({item[6]: item[1]})
+            mail_map[item[0]]['mon']['msg'].append(email_text)
+
+        email_subject = '您监控类别中的物品大幅度降价了！'
         for item in items[1]:  # email, item_name, item_price, discount, item_id, column_id, last_price
+            if item[0] not in mail_map:
+                mail_map[item[0]] = {'alert': {'ids': [], 'msg': []}}
+
+            if 'alert' not in mail_map[item[0]]:
+                mail_map[item[0]]['alert'] = {'ids': [], 'msg': []}
+
+            mail_map[item[0]]['mon']['ids'].append(item[7])
             item_url = 'https://item.jd.com/' + str(item[6]) + '.html'
             email_text = '物品：' + item[1] + '，\n' + \
-                         '上次监控价格为：' + str(item[3]) + '，\n' + \
                          '现在价格为：' + str(item[4]) + '，\n' + \
+                         '上次监控价格为：' + str(item[3]) + '，\n' + \
                          '降价幅度为：' + str(100 * float(item[3])) + '折，赶紧购买吧！\n' + \
                          '子标题：' + item[2] + '，\n' + \
                          '历史最高价参考：' + str(item[8]) + '，\n' + \
                          '历史最低价参考：' + str(item[9]) + '，\n' + \
                          item_url
-            email_subject = '您监控类别中的物品大幅度降价了！'
-            try:
-                send_email = Mail(email_text, 'admin', 'user', email_subject, item[0])
-                send_email.send()
-                time.sleep(Email_TIME)
-            except:
-                logging.critical('Sent email failure, skip in this loop: %s', item[0])
-                continue
 
-            sq.update_status(item[7])
-            logging.warning('Sent alert email SUCCESS: %s', item[0])
+            items_stdout.append({item[6]: item[1]})
+            mail_map[item[0]]['alert']['msg'].append(email_text)
+
+            
+        logging.warning('This loop sent email: %s', items_stdout)
+
+        items_processed = {'s': [], 'f': []}
+        for user, msg in mail_map.items():
+            for type, msg_text in msg.items():
+                if type == 'mon':
+                    email_subject = '您监控的物品有变更！'
+
+                elif type == 'alert':
+                    email_subject = '您监控类别中的物品大幅度降价了！'
+
+                try:
+                    send_email = Mail('\n\n\n'.join(msg_text['msg']), 'admin', 'user', email_subject, user)
+                    send_email.send()
+                    items_processed['s'] = items_processed['s'] + msg_text['ids']
+                    time.sleep(Email_TIME)
+                except Exception as e:
+                    logging.critical('Sent email failure with error: %s, skip in this loop: %s', e, user)
+                    items_processed['f'] = items_processed['f'] + msg_text['ids']
+                    continue
+
+            logging.warning('Finish sending email to user: %s', user)
+        
+        sq.update_status(items_processed['f'], 1)
+        sq.update_status(items_processed['s'], 0)
 
     def run(self):
         while True:
             items = self._check_item()  # dict of create_db.Monitor object
             items_info = CRAWLER_POOL.map(self._item_info_update, items)  # return two values as a tuple
-            logging.warning('This loop updated information: %s', items_info)
+            logging.warning('This loop updated information: %s', [{item['item_id']: item['item_name']} for item in items_info])
             self._send_email(items, items_info)
             time.sleep(ITEM_CRAWL_TIME)
 
